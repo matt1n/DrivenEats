1bf5a330db39b2178824c353d04636c4d0670304
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-12 16:49:28 -0300

    Botão mudando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function selecionarPrato(classe){
-    const botaoselect = document.querySelector('.pratos-principais .selecionado');
+    const botaoselect = document.querySelector('.pratosPrincipais .selecionado');
 
     if (botaoselect!==null){
         botaoselect.classList.remove('selecionado');
@@ -29,4 +29,16 @@ function selecionarSobremesa(classe){
 
     const botao = document.querySelector(classe);
     botao.classList.add('selecionado')
+}
+function teste() {
+    const botaoselect1 = document.querySelector('.bebidas .selecionado');
+    const botaoselect2 = document.querySelector('.sobremesas .selecionado');
+    const botaoselect = document.querySelector('.pratosPrincipais .selecionado');
+
+    if (botaoselect!==null && botaoselect1!==null && botaoselect2!==null){
+        const botao = document.querySelector('.terminar');
+        botao.classList.add('botaoON');
+        botao.innerHTML="Fechar pedido"
+        botao.classList.remove('terminar')
+    }
 }
