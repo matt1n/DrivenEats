d3b4a407f44d80b6fa7a0d1b21bcfdad14ca3bf2
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-15 13:02:13 -0300

    Bonus 2 + pequenos ajustes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// --------------- Selecionam as opções---------------
+
 function selecionarPrato(classe){
     const botaoselect = document.querySelector('.pratosPrincipais .selecionado');
     const iconeselec = document.querySelector('.pratosPrincipais .selecionado .icon');
@@ -43,6 +45,8 @@ function selecionarSobremesa(classe){
     icone.classList.remove('esconde');
 }
 
+// ---------------Troca os Botões---------------
+
 function switchBotao() {
     const botaoselect1 = document.querySelector('.bebidas .selecionado');
     const botaoselect2 = document.querySelector('.sobremesas .selecionado');
@@ -56,6 +60,61 @@ function switchBotao() {
     }
     
 }
+
+//---------------Abre aba de confirmação---------------
+
+function confirmar(){
+    let confirmacao = document.querySelector('.confirmacao')
+    confirmacao.classList.remove('confirmacaoSome')
+
+
+
+    let pratoEscolhido = document.querySelector('.pratosPrincipais .selecionado h3');
+    let prato = pratoEscolhido.innerHTML;
+    let bebidaEscolhida = document.querySelector('.bebidas .selecionado h3');
+    let bebida = bebidaEscolhida.innerHTML;
+    let sobremesaEscolhida = document.querySelector('.sobremesas .selecionado h3');
+    let sobremesa = sobremesaEscolhida.innerHTML;
+    
+    let pratoPreco=document.querySelector('.pratosPrincipais .selecionado .precoCheck p');
+    let pprato = String(pratoPreco.innerHTML);
+    pprato=pprato.replace('R$', '');
+    pprato=pprato.replace(',','.');
+    pprato=Number(pprato);
+    let bebidaPreco=document.querySelector('.bebidas .selecionado .precoCheck p');
+    let pbebida = String(bebidaPreco.innerHTML);
+    pbebida=pbebida.replace('R$', '');
+    pbebida=pbebida.replace(',','.');
+    pbebida=Number(pbebida);
+    let sobremesaPreco=document.querySelector('.sobremesas .selecionado .precoCheck p');
+    let psobremesa = String(sobremesaPreco.innerHTML);
+    psobremesa=psobremesa.replace('R$', '');
+    psobremesa=psobremesa.replace(',','.')
+    psobremesa=Number(psobremesa)
+    
+    let total = pprato+pbebida+psobremesa;
+
+
+
+
+    let pratoS = document.querySelector('.pratoSelect .escolhido');
+    pratoS.innerHTML = prato
+    let bebidaS = document.querySelector('.bebidaSelect .escolhido');
+    bebidaS.innerHTML = bebida
+    let sobremesaS = document.querySelector('.sobremesaSelect .escolhido');
+    sobremesaS.innerHTML = sobremesa
+
+    let pratoP = document.querySelector('.pratoSelect .preco');
+    pratoP.innerHTML = pprato.toFixed(2).replace(".",",")
+    let bebidaP = document.querySelector('.bebidaSelect .preco');
+    bebidaP.innerHTML = pbebida.toFixed(2).replace(".",",")
+    let sobremesaP = document.querySelector('.sobremesaSelect .preco');
+    sobremesaP.innerHTML = psobremesa.toFixed(2).replace(".",",")
+    let totalp = document.querySelector('.total .preco')
+    totalp.innerHTML = `R$ ${total.toFixed(2).replace(".",",")}`
+}
+
+//---------------Finaliza o pedido---------------
 
 function finalizar() {
     let pratoEscolhido = document.querySelector('.pratosPrincipais .selecionado h3');
@@ -98,4 +157,11 @@ Endereço: ${endereco}`
 
     let encodedpedido = encodeURIComponent(pedido)
     window.open(`https://wa.me/5522981818323?text=${encodedpedido}`)
+}
+
+// ---------------Fecha a aba de confirmação---------------
+
+function cancel () {
+    let confirmacao = document.querySelector('.confirmacao')
+    confirmacao.classList.add('confirmacaoSome')
 }
